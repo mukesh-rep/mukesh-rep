5433738c7fa51f277b4546510c3e5da409c648f0
Mukesh Singh <mukesh.singh@factset.com>
2020-04-07 11:12:31 +0530

    My commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { XyzComponent } from './xyz/xyz.component';
+import {MatToolbarModule } from '@angular/material/toolbar'
+import {MatCardModule} from '@angular/material/card'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
